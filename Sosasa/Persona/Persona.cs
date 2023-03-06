8ccc2b039bace38acf5344009253983dd9fef70d
BrayanSosaAlberto <125327983+BrayanSosaAlberto@users.noreply.github.com>
2023-03-06 16:53:18 -0600

    asincrona

---
+++ Sosasa/Persona/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sosasa.Persona
+{
+    internal class Persona
+    {
+    }
+}
